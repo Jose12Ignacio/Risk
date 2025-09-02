36d8270f03b95d90a19e897ff1dbd92be19fc584
Daniel-2473 <daniel.ulate937@gmail.com>
2025-09-01 20:53:32 -0700

    Login terminado
    Cambia de escena cuando presiona el boton

---
+++ Risk/Assets/Scripts/Login_manager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Login_manager : MonoBehaviour
+{
+    public TMP_InputField username;
+    public TMP_InputField ip;
+
+    public void SetPlayerData()
+    {
+
+        if (string.IsNullOrWhiteSpace(username.text))
+        {
+            Debug.Log("Ip o nombre invalido");
+        }
+        else
+        {
+            User_info.username = username.text;
+            User_info.ip = ip.text;
+            Debug.Log(User_info.username);
+            Debug.Log(User_info.ip);
+            Debug.Log("Informacion seteada");
+
+            SceneManager.LoadScene("Game_room");
+        }
+    }
+}
